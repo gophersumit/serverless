9607beee62d4fa51d6d1789506f00e74fd9762ab
sumit agrawal <sumitagrawal@sumits-Mac-mini.local>
2023-10-27 11:15:23 +0530

    Initial commit

---
+++ test/event-recol-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventRecolProject from '../lib/event-recol-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event-recol-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventRecolProject.EventRecolProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
